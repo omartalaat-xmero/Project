28dd04e2e561e12ec585ad7941ee2d5ac7df0163
omartalaat-xmero <136380940+omartalaat-xmero@users.noreply.github.com>
2024-12-07 23:17:58 +0200

    instructorsystem.java

--- instructorsystem.java
+++ instructorsystem.java
@@ -12,23 +12,21 @@ import java.util.List;
  * @author MAHMOUD TALAAT
  */
 public class instructorsystem {
-     private static final List<Instructor> inst = new ArrayList<>();
+    private static final List<Instructor> inst = new ArrayList<>();
     private static int totalInstructor = 0;
-    public static void addInstructor(int id,String name,String nameofcourses) {
+    public static void addInstructor(int id,String name,List<String> nameofcourses) {
         Instructor newinst = new Instructor(id, name,nameofcourses);
         inst.add(newinst);
         totalInstructor++;
-        System.out.println("Instructor Added\n");
-        newinst.instructortinfo();
+        System.out.println("Instructor Added\n"+newinst.instructortinfo());
     }
-    public void editInstructor(int id,String name,String nameofcourses) {
+    public static void editInstructor(int id,String name,List<String> nameofcourses) {
         boolean found = false;
         for(int i = 0; i < inst.size(); i++) {
             if (inst.get(i).getId()== id) {
                 inst.get(i).setName(name);
                 inst.get(i).setNameofcourses(nameofcourses);
-                System.out.println("Instructor updated");
-                inst.get(i).instructortinfo();
+                System.out.println("Instructor updated"+inst.get(i).instructortinfo());
                 found = true;
                 break;
             }
@@ -37,7 +35,7 @@ public class instructorsystem {
             System.out.println("Instructor with ID " + id + " not found.");
         }
     }
-    public void removeInstructor(int id) {
+    public static void removeInstructor(int id) {
         boolean removed = inst.removeIf(instructor -> instructor.getId() == id);
         if (removed) {
             System.out.println("Instructor with ID " + id + " removed.");
@@ -47,14 +45,16 @@ public class instructorsystem {
         }
     }
     public static void viewInstructor(int id) { 
+        boolean found = false;
         for (int i = 0; i < inst.size(); i++) {
             if (inst.get(i).getId() == id) { 
                 inst.get(i).instructortinfo();
-            }
-            else {
-                System.out.println("Instructor ID not found");
+                found = true;
+                break;
             }
         }
+        if (!found) {
+            System.out.println("Instructor with ID " + id + " not found.");
+         }
     }
 }
-
