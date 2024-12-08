426d8e8c90b86721ced1c0bb6215cfdccb266a06
Basmala239 <153266786+Basmala239@users.noreply.github.com>
2024-12-08 13:09:46 +0200

    Add files via upload

---
+++ person.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.task;
+
+/**
+ *
+ * @author Basmala
+ */
+public abstract class person {
+     private String name;
+    private String id;
+
+    // Constructor
+    public person(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    // Getter and Setter Methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public abstract void displayDetails();
+    
+}
