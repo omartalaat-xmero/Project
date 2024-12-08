d77b2510e8d3e2fc669b36ccdf3e6b187a3a2804
Basmala239 <153266786+Basmala239@users.noreply.github.com>
2024-12-08 13:09:18 +0200

    Update Course.java

--- Course.java
+++ Course.java
@@ -3,11 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.task;
+import java.util.*;
 
 /**
  *
  * @author Basmala
  */
 public class Course {
-    
+    private String courseName;
+    private String courseCode;
+    private List<Student> students;
+
+    public Course(String courseName, String courseCode) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
 }
+
