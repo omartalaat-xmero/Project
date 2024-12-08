e3951acc46f86f3496273683b7da9a1052ca86db
Basmala239 <153266786+Basmala239@users.noreply.github.com>
2024-12-08 13:05:24 +0200

    Update courses.java

--- courses.java
+++ courses.java
@@ -9,60 +9,18 @@ package com.mycompany.task;
  * @author MAHMOUD TALAAT
  */
 public class courses {
-    int numberOfcourses=0;
-    String course[]=new String[100];
+    List<Course>course=new ArrayList<>();
     
     
-    void insertCourse(String c){
-        course[numberOfcourses]=c;
-        numberOfcourses++;
+    void insertCourse(Course c){
+        course.add(c);
     }
     
-    void deleteCourse(String c){
-        boolean find=false;
-        for(int i=0;i<numberOfcourses;i++){
-            if(course[i]==c){
-                find=true;
-            }
-            if(find){
-                if(i<numberOfcourses-1)
-                    course[i]=course[i+1];
-                else
-                    course[i]="";
-            }
-        }
-        if(find)
-            System.out.println("Done");
-        else{
-            System.out.println("Not found");
-        }
-    }
-    
-    void editCourse(String c,String newC){
-         boolean find=false;
-        for(int i=0;i<numberOfcourses;i++){
-            if(course[i]==c){
-                find=true;
-                course[i]=newC;
-            }
-        }
-        if(find)
-            System.out.println("Done");
-        else{
-            System.out.println("Not found");
-        }
+    void deleteCourse(Course c){
+        course.remove(c);
     }
     
     void printCourses(){
-        if(numberOfcourses==0){
-            System.out.println("Empty");
-        }else{
-            System.out.print("{ ");
-            for(int i=0;i<numberOfcourses-1;i++){
-            System.out.print(course[i]+" ,");
-            }
-            System.out.println(course[numberOfcourses]+" }");
-        }
+        System.out.print(course);
     }
-    
 }
