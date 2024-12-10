134d347b33e4ede57bd83dd611b2b206943b5812
Basmala239 <153266786+Basmala239@users.noreply.github.com>
2024-12-10 18:11:25 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,20 +13,29 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        
-        /******************************student interface**************************************************/
-        studentsystem.addStudent(1203, "omar talaat", 70, "math");
-        studentsystem.addStudent(1204, "talaat", 80, "c++");
-       // studentsystem.editStudent(1203, "talaat","c++",60);
+        Course c=new Course("java","CS103"),c1=new Course("mathI","ms203"),
+               c2=new Course("Compiler constraction","CS203"),c3=new Course("Computer vision","CS109");
+        courses cs=new courses();
+        cs.insertCourse(c);
+        cs.insertCourse(c1);
+        cs.insertCourse(c2);
+        cs.insertCourse(c3);
+        Grade g=new Grade();
+       /******************************student interface**************************************************/
+        studentsystem.addStudent(1203, "omar talaat");
+        studentsystem.addStudent(1204, "talaat");
+        studentsystem.editStudent(1203, "talaat",c,g);
         studentsystem.removeStudent(1203);
         studentsystem.viewStudent(1204);
-        /*********************************************************************************************/
- /********************************Instructor interface****************************************/
-        instructorsystem.addInstructor(1, "talaat",Arrays.asList("java", "c++"));
-        instructorsystem.addInstructor(2, "talaat", Arrays.asList("c++", "math"));
+        /********************************Instructor interface****************************************/
+        instructorsystem.addInstructor(1, "talaat",Arrays.asList(c1,c2));
+        instructorsystem.addInstructor(2, "talaat",Arrays.asList(c3,c1));
         instructorsystem.viewInstructor(1);
-        instructorsystem.editInstructor(1, "talaat", Arrays.asList("c++", "math"));
+        instructorsystem.addInstructorCourse(1,c);
+        Instructor s=new Instructor(3,"Basmala",Arrays.asList(c3,c1));
+        s.addCourses(c);
+        System.out.print(s.displayDetails());
+               
         instructorsystem.removeInstructor(2);
-/*********************************************************************************************/
     }
 }
